0e35f81b8e1b6d61db1831cf0a5af019f66ce9bb
mmorales68 <144576312+mmorales68@users.noreply.github.com>
2024-05-04 09:28:20 -0600

    Incicializacion del repositorio con los archivos principales

---
+++ funciones.h
@@ -0,0 +1,14 @@
+//
+// Created by mmora on 4/05/2024.
+//
+
+#ifndef INC_2024_PARCIAL2_MORALES_MAIN_H
+#define INC_2024_PARCIAL2_MORALES_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //INC_2024_PARCIAL2_MORALES_MAIN_H
---
+++ main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mmora on 4/05/2024.
+//
+
+#include "main.h"
